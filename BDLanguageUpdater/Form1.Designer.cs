b77d9831a462b4224d639e4ca594653dc45b5619
Maxi <maximuller11@gmail.com>
2020-04-01 19:05:35 -0300

    Window icon change

--- BDLanguageUpdater/Form1.Designer.cs
+++ BDLanguageUpdater/Form1.Designer.cs
@@ -30,6 +30,7 @@ namespace BDLanguageUpdater
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
@@ -104,6 +105,7 @@ namespace BDLanguageUpdater
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "BDO Language Updater";
             this.Load += new System.EventHandler(this.Form1_Load);
