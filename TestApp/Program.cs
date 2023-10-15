375a5b6253ea9673e4f570eb719b8a8132b8f2d7
Santiago Agustin Gerhart <gerhartsantiago@gmail.com>
2023-10-15 03:35:26 -0300

    chore: update test app to use file manager

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -7,9 +7,11 @@ var FINAL_FILE_PATH = "languagedata_final.loc";
 var englishFileContent = File.ReadAllBytes(ENGLISH_FILE_PATH);
 var spanishFileContent = File.ReadAllBytes(SPANISH_FILE_PATH);
 
-var englishTSV = await LocSerializer.Decompress(englishFileContent);
-var spanishTSV = await LocSerializer.Decompress(spanishFileContent);
-var finalTsv = DictionaryUtils.Merge(englishTSV, spanishTSV);
+var locSerializer = new LocSerializer();
 
-var finalFile = await LocSerializer.Compress(finalTsv);   
+var englishTSV = await locSerializer.Deserialize(typeof(string), englishFileContent, string.Empty);
+var spanishTSV = await locSerializer.Deserialize(typeof(string), spanishFileContent, string.Empty);
+var finalTsv = DictionaryUtils.Merge(englishTSV.ToString(), spanishTSV.ToString());
+
+var finalFile = await locSerializer.Serialize(finalTsv);   
 await File.WriteAllBytesAsync(FINAL_FILE_PATH, finalFile);
