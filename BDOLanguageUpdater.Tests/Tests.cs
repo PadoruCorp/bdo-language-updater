7ca6710dcfb24f3d4fa299fc22cff2b80ba25aab
Maxi Muller <maximuller11@gmail.com>
2023-10-22 20:45:14 -0300

    Download logic updated

--- BDOLanguageUpdater.Tests/Tests.cs
+++ BDOLanguageUpdater.Tests/Tests.cs
@@ -1,10 +1,7 @@
 using BDOLanguageUpdater.Service;
 using BDOLanguageUpdater.Service.Serializer;
-using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Options;
-using Padoru.Core.Files;
 
 namespace BDOLanguageUpdater.Tests;
 
@@ -32,57 +29,16 @@ public class Tests : BaseHostTests
     }
 
     [Fact]
-    public async Task LanguageFileUpdater_DownloadFile_ShouldReturnValidFile()
-    {
-        // Arrange
-        var languageFileUpdater = this.ServiceProvider!.GetRequiredService<LanguageFileUpdater>();
-        var fileManager = this.ServiceProvider!.GetRequiredService<IFileManager>();
-        var versionUri = Constants.HTTPS_STRING_PROTOCOL_HEADER + Constants.DEFAULT_VERSION_URL;
-        var version = await languageFileUpdater.GetVersion(versionUri);
-        var downloadedFileUri = Constants.LOCAL_NOT_SERIALIZED_TEST_PROTOCOL_HEADER +
-                                Path.Combine(Path.GetTempPath(), Constants.DOWNLOADED_FILE_NAME)
-                                    .Replace(Constants.DEFAULT_STRING_TO_REPLACE_ON_URL, version);
-        var finalFileUri = Constants.HTTPS_NOT_SERIALIZED_PROTOCOL_HEADER +
-                           Constants.DEFAULT_FILE_URL.Replace(Constants.DEFAULT_STRING_TO_REPLACE_ON_URL, version);
-
-        // Act
-        await languageFileUpdater.DownloadFile(finalFileUri, downloadedFileUri);
-
-        // Assert
-        Assert.True(await fileManager.Exists(downloadedFileUri));
-    }
-
-    [Fact]
-    public async Task LanguageFileUpdater_MoveFile_ShouldMoveFileCorrectly()
-    {
-        // Arrange
-        var languageFileUpdater = this.ServiceProvider!.GetRequiredService<LanguageFileUpdater>();
-        var fileManager = this.ServiceProvider!.GetRequiredService<IFileManager>();
-        var testFileContentA = "This is a test file A"u8.ToArray();
-        var testFileContentB = "This is a test file B"u8.ToArray();
-        var fileAUri = Constants.LOCAL_NOT_SERIALIZED_TEST_PROTOCOL_HEADER + Path.Combine(Path.GetTempPath(), "fileA");
-        var fileBUri = Constants.LOCAL_NOT_SERIALIZED_TEST_PROTOCOL_HEADER + Path.Combine(Path.GetTempPath(), "fileB");
-        await fileManager.Write(fileBUri, testFileContentA);
-        await fileManager.Write(fileAUri, testFileContentB);
-
-        // Act
-        await languageFileUpdater.MoveFile(fileAUri, fileBUri);
-
-        // Assert
-        Assert.True(await fileManager.Exists(fileAUri));
-        Assert.True(!await fileManager.Exists(fileBUri));
-    }
-
-    [Fact]
     public async Task LanguageFileUpdater_UpdateFile_ShouldUpdateFile()
     {
         // Arrange
         var languageFileUpdater = this.ServiceProvider!.GetRequiredService<LanguageFileUpdater>();
-        
+
         // Act
-        await languageFileUpdater.UpdateFile();
-        
+        var exception = Record.ExceptionAsync(languageFileUpdater.UpdateFile);
+
         // Assert
+        Assert.Null(exception);
     }
 
     [Fact]
